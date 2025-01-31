bc1902758df98fe14c723d614bacdc97edc3885d
Viktor <viktor-876@yandex.ru>
2025-01-31 17:17:57 +0300

    Добавляет графику для табов

--- source/js/main.js
+++ source/js/main.js
@@ -3,7 +3,33 @@
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
 
+const buttonList = document.querySelector('.price__tab-list');
+const buttons = document.querySelectorAll('.price__tab-btn');
 
 
 
-// console.log("HI");
+function test(evt) {
+    const element = evt.target.closest('.price__tab-btn');
+
+    if(element) {
+        console.log(evt.target.getAttribute('data-id'));
+
+        buttons.forEach((item) => {
+            item.classList.remove('price__tab-btn--active');
+        })
+
+        evt.target.classList.add('price__tab-btn--active');
+    }
+}
+
+
+
+buttonList.addEventListener('click', test);
+
+
+
+
+
+
+
+// console.log(buttons);
